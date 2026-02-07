a3dc4ca6152a5d0c2f1ff6c43b881af18dc3dbc3
udeshglekshmi <udesh4dev@gmail.com>
2026-02-07 17:59:10 +0530

    Create class based program has static and non-static method

---
+++ source/Decorators.py
@@ -0,0 +1,29 @@
+class Leapx_org():
+                 
+    def __init__(self,first,last,pay):
+        self.f_name = first
+        self.l_name = last
+        self.pay_amt = pay
+        self.full_name = first+" "+last
+        
+    def make_email(self):
+        return self.f_name+ "."+self.l_name+"@xyz.com"
+
+    def __add__(self,other):
+
+        result=self.pay_amt+other.pay_amt
+        return result
+    
+    @staticmethod
+    def welcomeMessage():
+        print('Welcome to Operator Overload')
+
+# Invoke method directly without object instance as the method is static
+Leapx_org.welcomeMessage()
+
+# This is non-static method with two object instance 
+L_obj1 = Leapx_org('mohit', 'RAJ', 60000)
+L_obj2 = Leapx_org('Ravender', 'Dahiya',70000)
+print(L_obj1+L_obj2)
+
+
